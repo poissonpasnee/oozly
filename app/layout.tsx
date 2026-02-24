27bf7bc02eb88f353ab7818484b8bdbf5d188be3
poissonpasnee <ifsiapp.dental168@passmail.com>
2026-02-24 23:51:01 +0100

    Add min-h-screen class to body element

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="fr">
-      <body>
+      <body className="min-h-screen">
         {children}
         <BottomNav />
       </body>
