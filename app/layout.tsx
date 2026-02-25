e288790c78ee5dc4955be2937785ba3972d20011
poissonpasnee <ifsiapp.dental168@passmail.com>
2026-02-25 15:13:54 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,21 @@
 // app/layout.tsx
 import './globals.css'
 import type { Metadata } from 'next'
+import BottomNav from '@/components/BottomNav'
 
 export const metadata: Metadata = {
   title: 'Oozly',
   description: 'Oozly',
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="fr" suppressHydrationWarning>
       <body className="min-h-screen bg-white text-gray-900 dark:bg-gray-900 dark:text-white">
         {children}
+
+        {/* Barre du bas partout */}
+        <BottomNav />
       </body>
     </html>
   )
