202a84b3bf65e49ef2d51b378b6f1c121dbf294e
poissonpasnee <ifsiapp.dental168@passmail.com>
2026-02-25 13:41:21 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  images: { unoptimized: true },
-  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
+  trailingSlash: true
 }
 
-module.exports = nextConfig
+export default nextConfig
