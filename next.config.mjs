cf424b738be5dc359476cf9850c4ede0329fcdf2
poissonpasnee <ifsiapp.dental168@passmail.com>
2026-02-25 13:47:03 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,13 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  images: {
-    unoptimized: true
-  },
-  trailingSlash: true
+  trailingSlash: true,
+
+  // GitHub Pages ne supporte pas l'optimisation d'images Next côté serveur
+  images: { unoptimized: true },
+
+  // Important pour que le build sorte bien un dossier /out
+  reactStrictMode: true,
 }
 
 export default nextConfig
