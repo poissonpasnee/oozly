01c4ceb3ed6b8143bdad77745a3a2e52eb44a3f4
poissonpasnee <ifsiapp.dental168@passmail.com>
2026-02-25 13:36:26 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  images: {
-    unoptimized: true,
-  },
-  // C'est ça qui manquait ! Remplacez 'oozly-app' par le nom de votre repo
-  basePath: '/oozly',
-  assetPrefix: '/oozly',
-};
+  images: { unoptimized: true },
+  trailingSlash: true,
+}
 
-export default nextConfig;
+module.exports = nextConfig
