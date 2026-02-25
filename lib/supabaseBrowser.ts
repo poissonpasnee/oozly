e69e4bf80134359e6d396adef2db3fb7f91fcb79
poissonpasnee <ifsiapp.dental168@passmail.com>
2026-02-25 18:12:40 +0100

    Update supabaseBrowser.ts

--- lib/supabaseBrowser.ts
+++ lib/supabaseBrowser.ts
@@ -1,14 +1,13 @@
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = 'https://yafcbmyjjevmygwulupl.supabase.co'
-const SUPABASE_ANON_KEY =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlhZmNibXlqamV2bXlnd3VsdXBsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUwOTk0ODUsImV4cCI6MjA4MDY3NTQ4NX0.3onnOvuTJYJE-qtOglB5krBHtf6U0cNu4P6KSGbJqRQ'
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
-  auth: {
-    persistSession: true,
-    autoRefreshToken: true,
-    detectSessionInUrl: true,
-    storageKey: 'oozly-auth',
-  },
-})
+export const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+  {
+    auth: {
+      persistSession: true,
+      autoRefreshToken: true,
+      detectSessionInUrl: true
+    }
+  }
+)
